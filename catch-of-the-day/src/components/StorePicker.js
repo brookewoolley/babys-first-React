d83e67e188f94156ca82c39d6921d939e594057a
Brooke Woolley <brooke.woolley@gmail.com>
2019-05-08 14:07:05 +0100

    :construction: updated StorePicker class with onSubmit listener and routing

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // Stops the form from submitting and refreshing the page
+    event.preventDefault();
+    // gets the text from the inputn (ie the store name)
+    console.log(this.myInput.current.value);
+    // changes page to the relevant store
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store:</h2>
-        <input type="text" required placeholder="Store name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
-    )
+    );
   }
 }
 
